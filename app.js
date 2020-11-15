bb22a6404b3888e8b0fc65b1db42c9374c0395af
MaazKhan01 <59374050+MaazKhan01@users.noreply.github.com>
2020-11-15 15:32:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log(firebase);
+function todo() {
+    //making task node
+    if (document.getElementById('task').value == "") {
+
+        alert('fill the text area!')
+        return false;
+
+    } else {
+
+        var val = document.getElementById('task');
+        var list = document.createElement('li');
+        var text = document.createTextNode(val.value);
+        list.appendChild(text);
+        //deleting item
+        var del = document.createElement('button');
+        var delTxt = document.createTextNode('DELETE');
+        del.setAttribute('class', 'btn')
+        del.setAttribute('onclick', 'delITEM(this)')
+
+        del.appendChild(delTxt);
+        list.appendChild(del);
+        var messages = document.getElementById("text_area");
+        messages.appendChild(list)
+        val.value = "";
+    }
+
+}
+function delITEM(p) {
+    p.parentNode.remove();
+}
+//delete all nodes by pressing delete all button
+function delAll() {
+    text_area.innerHTML = "";
+}
